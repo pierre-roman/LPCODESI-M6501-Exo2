0eb166e203cbe894f31a97127ae8e493e10313bd
Pierre <flaeur@gmail.com>
2019-02-15 15:50:54 +0100

    classe Compte et implementation de autorise paiement dans carteDebit

--- module/src/CarteDebit.java
+++ module/src/CarteDebit.java
@@ -1,5 +1,7 @@
 public class CarteDebit extends CartePaiement{
+    protected Compte compteAssocie;
     @Override
     public boolean autorisePaiement(int montant) {
+        return (compteAssocie.solde >= montant);
     }
 }
---
+++ module/src/Compte.java
@@ -0,0 +1,3 @@
+public class Compte {
+    public int solde;
+}
